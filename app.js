2124ddc1dcc73f37292321c57bb1333d591db98e
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2023-01-10 12:32:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-// Create Interface where we can enter the number without stopping the program:
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-// A Random Method which will help us choose a random number:
-let compGuess = Math.floor(Math.random() * 100);
+const { getUnpackedSettings } = require('http2');
+function guessNumber() {
+    // Create Interface where we can enter the number without stopping the program:
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    // A Random Method which will help us choose a random number:
+    let compGuess = Math.floor(Math.random() * 100);
+    let guess;
+
+    // Function that stores the question:
+    let recursiveAsyncReadLine = function () {
+        readline.question('Guess the number(1 - 100): ', number => {
+            guess = Number(number);
+
+            // Check Player's Input:
+            if (guess <= 100 && guess >= 0) {
+                if (guess === compGuess) {
+                    console.log('You guessed it!');
+                    return readline.close();
+                } else if (guess > compGuess) {
+                    console.log('Too high!');
+                    recursiveAsyncReadLine();
+                } else if (guess < compGuess) {
+                    console.log('Too low!');
+                    recursiveAsyncReadLine();
+                }
+            } else {
+                console.log('Invalid input! Please try again...');
+                recursiveAsyncReadLine();
+            }
+        });
+    }
+    recursiveAsyncReadLine();
+}
+
+guessNumber();
 
